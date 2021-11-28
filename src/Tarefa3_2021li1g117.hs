3c1eaac5a2ce7d7ea79e7e937378f4b54339e481
a100896 <a100896@alunos.uminho.pt>
2021-11-28 12:59:10 +0000

    tarefa3

--- src/Tarefa3_2021li1g117.hs
+++ src/Tarefa3_2021li1g117.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 {- |
 Module      : Tarefa3_2021li1g117
 Description : Representação textual do jogo
@@ -11,3 +12,17 @@ import LI12122
 
 instance Show Jogo where
   show = undefined
+
+mapaString :: Mapa -> String
+mapaString l = juntarStrings (map linhaMapaString l) 
+  where linhaMapaString :: [Peca]  -> String
+        linhaMapaString [] = ""
+        linhaMapaString (x : y)
+          | x == Caixa = "C" ++ linhaMapaString y
+          | x == Bloco = "X" ++ linhaMapaString y 
+          | x == Porta = "P" ++ linhaMapaString y 
+          | otherwise = " " ++ linhaMapaString y 
+
+juntarStrings :: [String] -> String
+juntarStrings [] = ""
+juntarStrings (x : y) = x ++ juntarStrings y 
