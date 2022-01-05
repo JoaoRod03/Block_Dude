f613cf50f5fa2ab9c64b154520923aeb3832728d
a100896 <a100896@alunos.uminho.pt>
2022-01-05 11:49:19 +0000

    mapa

---
+++ src/Mapa.hs
@@ -0,0 +1,20 @@
+
+module Mapa where
+
+import Tarefa1_2021li1g117
+import Tarefa2_2021li1g117
+import Tarefa3_2021li1g117
+import Tarefa4_2021li1g117
+import LI12122
+
+
+
+mapaNivel :: Mapa
+mapaNivel = [[Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio],
+            [Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio],
+            [Porta,Vazio,Bloco,Bloco,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio],
+            [Bloco,Bloco,Bloco,Bloco,Bloco,Vazio,Vazio,Vazio,Vazio,Vazio],
+            [Bloco,Bloco,Vazio,Vazio,Vazio,Bloco,Vazio,Vazio,Vazio,Vazio],
+            [Bloco,Bloco,Vazio,Vazio,Vazio,Bloco,Vazio,Vazio,Vazio,Vazio],
+            [Bloco,Bloco,Vazio,Vazio,Vazio,Bloco,Bloco,Bloco,Vazio,Caixa],
+            [Bloco,Bloco,Bloco,Bloco,Bloco,Bloco,Bloco,Bloco,Bloco,Bloco]]
