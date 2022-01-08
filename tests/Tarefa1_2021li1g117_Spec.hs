c7aae765d15f33e55530eb3a03a0c5284caddf8b
a100896 <a100896@alunos.uminho.pt>
2022-01-08 10:56:18 +0000

    alguns testes

--- tests/Tarefa1_2021li1g117_Spec.hs
+++ tests/Tarefa1_2021li1g117_Spec.hs
@@ -12,16 +12,10 @@ testsT1 =
     [ "Tarefa 1 - Teste Valida Mapa m1r" ~: validaPotencialMapa m1 ~=? True
     , "Tarefa 1 - Teste Valida Mapa vazio" ~: validaPotencialMapa [] ~=? False
     , "Tarefa 1 - Teste Valida Mapa com 2 portas" ~: validaPotencialMapa [(Porta, (0,0)), (Porta, (1,0))] ~=?  False
+    , "Tarefa 1 - Teste Valida Mapa sem porta " ~: validaPotencialMapa [(Caixa,(1,3)),(Bloco,(1,4))] ~=? False
+    , "Tarefa 1 - Teste Valida Mapa valido " ~: validaPotencialMapa [(Porta, (0, 2)),(Bloco, (0, 3)), (Bloco, (1, 3)), (Bloco, (2, 3))] ~=? True
+    , "Tarefa 1 - Teste Valida Mapa com uma caixa a flutuar" ~: validaPotencialMapa [(Porta,(0,2)),(Bloco,(0,3)),(Bloco, (1,3)),(Bloco,(2,3)),(Caixa,(1,0))] ~=? False
+    , "Tarefa 1 - Teste Valida Mapa só com blocos e porta" ~: validaPotencialMapa [(Porta,(0,0)),(Bloco,(0,1)),(Bloco,(1,0)),(Bloco,(1,1))] ~=? False
     ]
-
-
-
-"Teste Valida Mapa vazio" ~ validaPotencialMapa [] = False
-
-"Teste Valida Mapa com 2 portas " ~ validaPotencialMapa [(Porta, (0,0)), (Porta, (1,0))] = False
-
-"Teste Valida Mapa sem porta " ~validaPotencialMapa [(Caixa,(1,3)),(Bloco,(1,4))] = False
-
-"Teste Valida Mapa valido " ~validaPotencialMapa [(Porta, (0, 2)),(Bloco, (0, 3)), (Bloco, (1, 3)), (Bloco, (2, 3))] = True
 
 
--- tests/Tarefa2_2021li1g117_Spec.hs
+++ tests/Tarefa2_2021li1g117_Spec.hs
@@ -14,13 +14,7 @@ testsT2 =
     , "Tarefa 2 - Teste Desconstruir Mapa vazio" ~: [] ~=? desconstroiMapa []
     , "Tarefa 2 - Teste Identidade m1" ~: sort m1 ~=?  sort (desconstroiMapa (constroiMapa m1))
     , "Tarefa 2 - Teste Identidade m1r" ~: m1r ~=?  constroiMapa (desconstroiMapa m1r)
-    , "Tarefa 2 - Teste Construir Sobrepor Peças" ~: constroiMapa [(Porta, (7, 4))] ~=?  constroiMapa [(Porta, (7, 4)), (Porta, (7, 4))]
+    , "Tarefa 2 - Teste Construir Sobrepor Peças" ~: constroiMapa [(Porta, (7, 4)),(Porta, (7, 4))] ~=?  constroiMapa [(Porta, (7, 4)), (Porta, (7, 4))]
     ]
 
-
-constroiMapa [] = []
-
-sort (desconstroiMapa m1r) =[(Bloco,(0,4)),(Bloco,(1,4)),(Bloco,(2,4)),(Bloco,(3,4)),(Bloco,(4,4)),(Bloco,(5,4)),(Bloco,(6,1)),(Bloco,(6,2)),(Bloco,(6,3)),(Bloco,(6,4)),(Caixa,(4,3)),(Porta,(0,3))]
-
-desconstroiMapa [] = []
 
